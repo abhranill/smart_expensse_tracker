6c111e0d34587648101cde9534060ff74b7d86b1
Abhranil Roy <abhranil.roy07@gmail.com>
2026-02-14 00:27:11 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API Running 🔥");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected Successfully 🚀"))
+  .catch((err) => console.log("MongoDB Error:", err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
