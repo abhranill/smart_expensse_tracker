f2c9dfd98ae9e4bc4fc66247ac99524b7e323b1d
Abhranil Roy <abhranil.roy07@gmail.com>
2026-02-14 22:14:42 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpense = () => {
+    if (!title || !amount) return;
+
+    const newExpense = {
+      title,
+      amount
+    };
+
+    setExpenses([...expenses, newExpense]);
+    setTitle("");
+    setAmount("");
+  };
+  const deleteExpense = (indexToDelete) => {
+  const updatedExpenses = expenses.filter(
+    (_, index) => index !== indexToDelete
+  );
+  setExpenses(updatedExpenses);
+};
+
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>💰 Smart Expense Tracker</h1>
+
+      <h2>Add Expense</h2>
+
+      <input
+        type="text"
+        placeholder="Expense Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        style={{ padding: "8px", marginRight: "10px" }}
+      />
+
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        style={{ padding: "8px", marginRight: "10px" }}
+      />
+
+      <button onClick={addExpense} style={{ padding: "8px 15px" }}>
+        Add
+      </button>
+
+      <h2 style={{ marginTop: "40px" }}>Expenses</h2>
+
+      <ul>
+  {expenses.map((exp, index) => (
+    <li key={index}>
+      {exp.title} - ₹{exp.amount}
+      <button onClick={() => deleteExpense(index)}>
+        ❌
+      </button>
+    </li>
+  ))}
+   </ul>
+
+      <h3>
+  Total: ₹
+  {expenses.reduce((total, exp) => total + Number(exp.amount), 0)}
+</h3>
+
+    </div>
+  );
+}
+
+export default App;
+
+
